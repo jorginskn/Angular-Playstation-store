9175d50461ce467c5fd629f744a07f907353a5fe
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2022-12-21 17:25:35 -0300

    fit: estilização de card , label , pricing

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
   templateUrl: './card-label.component.html',
-  styleUrls: ['./card-label.component.css']
+  styleUrls: ['./card-label.component.css'],
 })
-export class CardLabelComponent {
-
+export class CardLabelComponent implements OnInit {
+  @Input() gameLabel: string = '';
+  ngOnInit() {}
 }
--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  styleUrls: ['./card-pricing.component.css'],
 })
-export class CardPricingComponent {
-
+export class CardPricingComponent implements OnInit {
+  @Input() gameType: string = 'Digital PS4';
+  @Input() gamePrice: string = 'R$399,90';
+  ngOnInit(): void {}
 }
